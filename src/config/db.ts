effc4476f81391eddffda18cbafa261521df5a81
kavita sharma <115460412+Kavitadsharma@users.noreply.github.com>
2023-12-13 13:12:33 +0530

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose,{connect} from "mongoose";
 
 
 function connects(){
-    return connect("mongodb+srv://kavitadsharma899107:kavita8991@cluster0.junbtiy.mongodb.net/typescript?retryWrites=true&w=majority")
+    return connect("mongodb+srv://kavitadsharma899107:<password>@cluster0.junbtiy.mongodb.net/typescript?retryWrites=true&w=majority")
     .then(()=>{
         console.log("db connected")
     }).catch((error:any)=>{
